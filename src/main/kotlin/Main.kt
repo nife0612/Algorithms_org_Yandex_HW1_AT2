aca44c8007a14fa7ebd1cd7788adec88a2f998f5
Nife <nife0612@gmail.com>
2023-01-29 18:55:49 +0400

    Algorithms_org_Yandex_HW_2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,9 @@
 import java.util.Scanner
 
-fun main(args: Array<String>) {
+fun main() {
 
-    AT2_Exp_6()
+    HW2_C_Nearest_number()
+
 }
 
 // Legend:
@@ -132,6 +133,100 @@ fun HW1_D_equation_with_root() {
 // c > 0 and a!=0 and a=b=c=0 -> inf and x >= - b/a
 }
 
+fun HW1_E_Ambulance(){
+
+}
+
+fun HW1_F_Placement_of_laptops(){
+    val scanner = Scanner(System.`in`)
+
+    val list = mutableListOf<Int>()
+    val listSquares = mutableListOf<Int>()
+
+    for (i in 0 until 4)
+        list.add(scanner.nextInt())
+
+    var sideA : Int = 0
+    var sideB : Int = 0
+    var minSquare : Int = Int.MAX_VALUE
+
+    for (i in 0 until 2)
+        for (j in 2 until 4){
+            listSquares.add( (list[i]+list[j]) * max(list[1-i], list[5-j]) )
+            if(minSquare > listSquares[listSquares.size - 1]){
+                minSquare = listSquares[listSquares.size - 1]
+                sideA = (list[i]+list[j])
+                sideB = max(list[1-i], list[5-j])
+            }
+        }
+
+    println("$sideA $sideB")
+    //println(min(min(listSquares[0], listSquares[1]), min(listSquares[2], listSquares[3])))
+
+}
+
+fun max(a : Int, b : Int) : Int{
+    return if (a > b) a else b
+}
+fun min(a : Int, b : Int) : Int{
+    return if (b > a) a else b
+}
+
+fun HW1_G_Details(n : Int, k : Int, m : Int) : Int{
+    /*
+    val scanner = Scanner(System.`in`)
+
+
+    var N : Int = scanner.nextInt()
+    var K : Int = scanner.nextInt()
+    var M : Int = scanner.nextInt()
+    */
+
+    var N : Int = n
+    var K : Int = k
+    var M : Int = m
+
+    var a : Int     // a = N / K
+    var b : Int     // b = K / M
+    var n1: Int     // n1 = N % K
+    var n2: Int     // n2 = K % M
+
+    var ans : Int = 0
+
+    if(K in M..N) {
+        while (N / K > 0) {
+
+            a = N / K
+            n1 = N % K
+
+            b = K / M
+            n2 = a * (K % M)
+
+            N = n1 + n2
+            ans += a * b
+        }
+    }
+    //println(ans)
+    return ans
+}
+
+fun HW1_I_Prisoner_of_the_Chateau_d_If(){
+    val scanner = Scanner(System.`in`)
+
+    var flag = false
+    val list = mutableListOf<Int>()
+
+    for(i in 0 until 5)
+        list.add(scanner.nextInt())
+
+    for(i in 3 until 5){
+        if(list[1] <= list[i] && list[2] <= list[7-i]) flag = true
+        else if (list[1] <= list[i] && list[0] <= list[7-i]) flag = true
+        else if (list[0] <= list[i] && list[2] <= list[7-i]) flag = true
+    }
+
+    print(if(flag) "YES" else "NO")
+}
 
 // https://yandex.ru/yaintern/algorithm-training_1
 fun AT2_Exp_1(){
@@ -239,5 +334,99 @@ fun AT2_Exp_6(){
         if (i.length == minLen)
             print("$i ")
     }
+
+}
+
+fun AT2_Exp_8(){
+    val line = "AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
+
+    var ans = ""
+    var lastsym = line[0]
+    var repitions = 0
+
+    for(i in line){
+        if(lastsym != i){
+            ans += lastsym
+            if(repitions > 1){
+                ans += repitions
+            }
+            lastsym = i
+            repitions = 0
+        }
+
+        ++repitions
+    }
+
+    ans += lastsym
+    ans += repitions
+
+    println(ans)
+}
+
+fun HW2_A_Is_the_list_growing(){
+    val scanner = Scanner(System.`in`).nextLine().split(" ")
+    val list = mutableListOf<Int>()
+
+    for(element in scanner){
+        list.add(element.toInt())
+    }
+
+    var flag = true
+
+    for (i in 0 until list.size - 1)
+        if(list[i] >= list[i+1])
+        {
+            flag = false
+            break
+        }
+
+    if(flag) print("YES")
+    else print("NO")
+}
+
+fun HW2_B_Determine_the_type_of_sequence(){
+    val scanner = Scanner(System.`in`)
+    val list = mutableListOf<Int>()
+
+    var tmp : Int
+    while(true){
+        tmp = scanner.nextInt()
+        if(tmp == -2_000_000_000)
+            break
+        list.add(tmp)
+    }
+
+    var isConstant = true
+    var isAscending = true
+    var isDescending = true
+    var isWeakAscending = true
+    var isWeakDescending = true
+    var isRandom = true
+
+    for (i in 0 until list.size - 1){
+        if(list[i] != list[i+1])
+            isConstant = false
+        if(list[i] >= list[i+1])
+            isAscending = false
+        if(list[i] <= list[i+1])
+            isDescending = false
+        if(list[i] > list[i+1])
+            isWeakAscending = false
+        if(list[i] < list[i+1])
+            isWeakDescending = false
+    }
+
+    if(isConstant)  println("CONSTANT")
+    else if(isAscending)    println("ASCENDING")
+    else if(isDescending)   println("DESCENDING")
+    else if(isWeakAscending)    println("WEAKLY ASCENDING")
+    else if(isWeakDescending)   println("WEAKLY DESCENDING")
+    else println("RANDOM")
+
+
+
+}
+
+fun HW2_C_Nearest_number(){
 
 }
